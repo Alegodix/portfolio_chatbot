e4cdb93cbc239253e4b21adf8fbb44b4c22b7744
Alegodix <alexandre.godineau@etu.cyu.fr>
2025-09-04 14:54:15 +0200

    Modifications légères du site

--- script.js
+++ script.js
@@ -21,30 +21,35 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Mini "IA" par règles simples
+  appendMessage('bot', "Bonjour 👋 ! Je suis le mini-assistant d’Alexandre. Posez-moi une mini-question sur ses projets, son GitHub ou son contact.");
+
   function miniAI(message) {
     const msg = message.toLowerCase();
 
     const rules = [
-      { test: /^(bonjour|salut|bonsoir|hello)\b/i,
-        answer: "Bonjour 👋 ! Je suis le mini-assistant d’Alexandre. Posez-moi une mini-question sur ses projets, son GitHub ou son contact." },
+      { test: /^(bonjour|salut|bonsoir|hello|hi)\b/i,
+        answer: "N'hésitez pas à me poser une mini-question !" },
 
       { test: /(mail|email|contacter|contact)/,
         answer: "Voulez-vous écrire à <a href='mailto:alexandre_godineau@proton.me'>alexandre_godineau@proton.me</a>." },
+
+       { test: /(blender|3d|3D)/,
+        answer: "Il n'y a pas de code ou de dépôt GitHub pour ce projet" },
 
       { test: /\brunner\b/,
         answer: "Runner : jeu Unity (C#) avec animations et vitesse progressive. Code : <a href='https://github.com/Alegodix/Runner' target='_blank' rel='noopener'>GitHub</a>." },
 
       { test: /(multiverseus|tour[- ]?par[- ]?tour|\bjeu\b.*\bc\b)/,
-        answer: "MultiverSeus : combat tour-par-tour en C pur (sans moteur), à 3. Repo : <a href='https://github.com/cardotbutzneo/projet_info' target='_blank' rel='noopener'>ici</a>." },
+        answer: "MultiverSeus : combat tour-par-tour en C pur (sans moteur), à 3. Lien du code sur GitHub ici : <a href='https://github.com/cardotbutzneo/projet_info' target='_blank' rel='noopener'>ici</a>." },
 
       { test: /(portfolio|site|ce site)/,
-        answer: "Ce site est fait en HTML5/CSS. Voulez-vous des détails sur la stack ou le design ?" },
+        answer: "Ce site est fait en HTML5/CSS. Lien du code sur GitHub ici : <a href='https://github.com/Alegodix/portfolio_chatbot' target='_blank' rel='noopener'>Lien sur GitHub</a>." },
 
       { test: /\bgithub\b/,
         answer: "Le GitHub d’Alexandre : <a href='https://github.com/Alegodix' target='_blank' rel='noopener'>@Alegodix</a>." },
 
       { test: /\bcv\b|curriculum/,
-        answer: "Alexandre peut envoyer son CV sur demande. Laisse un mail ou contactez-le directement par email." },
+        answer: "Lien du CV d'Alexandre : <a href='https://cvdesignr.com/p/lkoXEjYQ744Qa39' target='_blank' rel='noopener'>Lien du CV</a>." },
     ];
 
     for (const r of rules) {
